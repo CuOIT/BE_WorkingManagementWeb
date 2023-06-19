674039e7bf8f9103f30ff5f9506938ce0ec564eb
Cường Ngô Đức <legendof2k2@gmail.com>
2023-06-19 09:50:33 +0700

    [CuO] remove error

--- src/routes/private/project.js
+++ src/routes/private/project.js
@@ -1,13 +1,12 @@
-const projectController=require('../../controllers/projectController')
+const projectController = require("../../controllers/projectController");
 
-
-module.exports = (router)=>{
-    router.put('/project/authorize',projectController.handleAuthorizeAdmin);
-    router.delete('/project/leave-project',projectController.handleLeaveFromProject);
-    router.post('/project',projectController.handleCretateProject);
-    router.get('/project',projectController.handleGetProjectbyId)
-    router.put('/project/:id',projectController.handleUpdateProject)
-    router.delete('/project/:id',projectController.handleDeleteProject)
-    router.post('/project/add-member',projectController.handleAddMember)
-    router.get('/project',projectController.handleGetUserByProjectId)
+module.exports = (router) => {
+    router.put("/project/authorize", projectController.handleAuthorizeAdmin);
+    router.delete("/project/leave-project", projectController.handleLeaveFromProject);
+    router.post("/project", projectController.handleCretateProject);
+    router.get("/project", projectController.handleGetProjectbyId);
+    router.put("/project/:id", projectController.handleUpdateProject);
+    router.delete("/project/:id", projectController.handleDeleteProject);
+    router.post("/project/add-member", projectController.handleAddMember);
+    router.get("/project", projectController.handleGetAllMemberOfProject);
 };
